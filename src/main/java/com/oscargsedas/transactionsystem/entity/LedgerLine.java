17cde1b84741d932c4dd670dd0036f00b59e8335
Oscar Gomez <oscargomezsedas@gmail.com>
2026-04-12 18:02:12 +0200

    feat: add LedgerLine entity for tracking ledger entries in transactions

---
+++ src/main/java/com/oscargsedas/transactionsystem/entity/LedgerLine.java
@@ -0,0 +1,31 @@
+package com.oscargsedas.transactionsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "ledger_lines")
+@RequiredArgsConstructor
+public class LedgerLine extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "id", nullable = false)
+	private UUID id;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "transaction_id", nullable = false)
+	private Transaction transaction;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "account_id", nullable = false)
+	private Account account;
+
+	private BigDecimal amount;
+}
