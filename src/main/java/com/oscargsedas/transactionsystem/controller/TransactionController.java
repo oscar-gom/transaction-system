45850d6f92456b6fc7f6a8f1ada89afb07d5e1fc
Oscar Gomez <oscargomezsedas@gmail.com>
2026-04-22 17:47:49 +0200

    feat: implement TransactionController for managing transactions with retrieval and creation endpoints

---
+++ src/main/java/com/oscargsedas/transactionsystem/controller/TransactionController.java
@@ -0,0 +1,57 @@
+package com.oscargsedas.transactionsystem.controller;
+
+import com.oscargsedas.transactionsystem.dto.ApiSuccessResponse;
+import com.oscargsedas.transactionsystem.dto.TransactionDto;
+import com.oscargsedas.transactionsystem.dto.TransactionRequest;
+import com.oscargsedas.transactionsystem.service.TransactionService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/v1/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+	private final TransactionService transactionService;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<ApiSuccessResponse<TransactionDto>> getTransactionById(
+			@PathVariable UUID id,
+			HttpServletRequest request
+	) {
+		TransactionDto transactionDto = transactionService.getTransactionById(id);
+
+		ApiSuccessResponse<TransactionDto> response = new ApiSuccessResponse<>(
+				Instant.now(),
+				HttpStatus.OK.value(),
+				"Transaction retrieved successfully",
+				request.getRequestURI(),
+				transactionDto
+		);
+
+		return ResponseEntity.ok(response);
+	}
+
+	@PostMapping("/create")
+	public ResponseEntity<ApiSuccessResponse<TransactionDto>> createTransaction(
+			@RequestBody TransactionRequest transactionRequest,
+			HttpServletRequest request
+	) {
+		TransactionDto transactionDto = transactionService.createTransaction(transactionRequest);
+
+		ApiSuccessResponse<TransactionDto> response = new ApiSuccessResponse<>(
+				Instant.now(),
+				HttpStatus.CREATED.value(),
+				"Transaction created successfully",
+				request.getRequestURI(),
+				transactionDto
+		);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+}
